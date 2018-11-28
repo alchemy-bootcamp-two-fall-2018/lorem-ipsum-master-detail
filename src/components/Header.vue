23e46248d4b128eacd494b86eca930c7b0df3345
Tyler Corbett <thetylercorbett@gmail.com>
2018-11-28 13:57:13 -0800

    header displays

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <img src="../assets/logo.png">
+        <h1>Ipsum Emporium</h1>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    header {
+        display: flex;
+        justify-content: flex-start;
+        padding: 20px;
+        height: 50px;
+        background: steelblue;
+        color: white;
+    }
+</style>
