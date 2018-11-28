44bcd2936b2178cb6cf3e827bea93c3d93ea3d1c
Paige Gorry <paigeegorry@gmail.com>
2018-11-28 13:45:01 -0800

    basic css styling for header and header completed for now

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,8 @@
 <template>
-    <h1>Hello?</h1>
+    <div id="header">
+        <h1>Ipsum Flipsum</h1>
+        <p>Everybody wants their own ipsum...</p>
+    </div>
 </template>
 
 <script>
@@ -9,5 +12,16 @@ export default {
 </script>
 
 <style>
-
+#header {
+    color: rgb(35, 35, 35);
+    margin: 0;
+    padding: 20px;
+    border: 1px navy solid;
+}
+h1 {
+    text-transform: uppercase;
+}
+p {
+    font-size: 1.15em;
+}
 </style>
