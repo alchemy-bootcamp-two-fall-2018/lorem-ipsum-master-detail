1cec3b6dd77e25261d333cc8ecd09fea7bc62a55
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-11-28 14:20:08 -0800

    header renders to page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Header/>
   </div>
 </template>
 
 <script>
 
+import Header from './components/Header.vue';
+
 export default {
     components: {
-  
+        Header
     }
 };
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        This is my Header
+    </header>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
