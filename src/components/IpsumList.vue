27486108e41a1d08716d35d29b9778863d1d6119
Shaba9 <shaba92@gmail.com>
2018-11-28 14:25:43 -0800

    some css on list items

--- src/components/IpsumList.vue
+++ src/components/IpsumList.vue
@@ -1,6 +1,7 @@
 <template>
     <ul>
         <li v-bind:key="ipsum.author" v-for="ipsum in ipsums">
+            <p><img v-bind:src="imageSrc"></p>
             <p>Author: {{ipsum.author}}</p>
             <p>Title: {{ipsum.title}}</p>
         </li>
@@ -9,6 +10,11 @@
 
 <script>
 export default {
+    data() {
+        return {
+            imageSrc: 'https://i.pinimg.com/originals/e1/9a/62/e19a628908e921664dd0fe79e26f1125.png'
+        };
+    },
     props: {
         ipsums: Array
     }
@@ -16,5 +22,20 @@ export default {
 </script>
 
 <style>
+img {
+    width: 100px;
+}
 
+ul {
+    list-style: none;
+}
+
+li {
+    display: inline-block;
+    margin: 1%;
+    padding: 5px;
+    border: 1px solid blue;
+    width: 8vw;
+    height: auto;
+}
 </style>
