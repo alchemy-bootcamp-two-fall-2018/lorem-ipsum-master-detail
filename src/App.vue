fe2ee74bb86f5b55f7062c0f1f8ae9e1c37d46e3
Tyler Corbett <thetylercorbett@gmail.com>
2018-11-28 14:18:37 -0800

    ipsums component displaying

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <Header/>
+    <Ipsums/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Ipsums from './components/Ipsums.vue';
 import ipsumApi from '../ipsumApi.js';
 export default {
   components: {
     Header,
+    Ipsums,
   }
 }
 </script>
