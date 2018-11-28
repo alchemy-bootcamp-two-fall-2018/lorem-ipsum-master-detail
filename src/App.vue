d3fdc5bb9c7ead93acb29437868097a8f5d86dc2
Cheri Portman <cheriportman@hotmail.com>
2018-11-28 14:30:52 -0800

    basic build begun

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <Header/>
+    </div>
+</template>
+
+<script>
+import Header from './Header.vue';
+
+export default {
+    components: {
+        Header
+    }
+
+};
+
+</script>
+
+<style>
+</style>
---
+++ src/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class='header'>
+        <p>Ipsums R Us</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+    .header {
+        background-color: blue;
+        color: red;
+        font-size: 36px;
+    }
+</style>
