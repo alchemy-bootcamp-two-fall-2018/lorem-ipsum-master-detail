21f9556435f789899129e314d07fe5fb7faa2598
Abel Quintero <abel.j.quintero@gmail.com>
2018-11-28 22:47:23 -0800

    form renders

--- src/components/IpsumForm.vue
+++ src/components/IpsumForm.vue
@@ -1,13 +1,51 @@
 <template>
-  <ul>
-        <h3>This is the ipsum form component</h3>
-  </ul>
+  <section>
+  <form @submit.prevent="onAdd(ipsum)">
+    <label>
+      <span>Name:</span>
+      <input v-model="ipsum.title" required>
+    </label>
+
+    <label>
+      <span>Author:</span>
+      <input v-model="ipsum.author" required>
+    </label>
+
+    <label>
+      <span>Category</span>
+      <input v-model="ipsum.category" required>
+    </label>
+
+    <label>
+      <span>Body:</span>
+      <input v-model="ipsum.body" required>
+    </label>
+
+    <label>
+      <span></span>
+      <button type="submit">Add</button>
+      
+    </label>
+
+  </form>
+  </section>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      ipsum: {
+        title: '',
+        author: '',
+      }
+    };
+  },
+  props: {
+    onAdd: Function,
+    onCancel: Function
+  }
+};
 </script>
 
 <style>
