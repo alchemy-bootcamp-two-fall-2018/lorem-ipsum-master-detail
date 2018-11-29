cac60f36663f726f0197db71d8dcf5e550c50c77
Paige Gorry <paigeegorry@gmail.com>
2018-11-28 18:10:46 -0800

    hide/show form button is working

--- src/components/Ipsum.vue
+++ src/components/Ipsum.vue
@@ -6,7 +6,11 @@
         <div v-if="ipsum">
             <IpsumDetail :ipsum="ipsum"/>
         </div>
-        <div>
+        <div v-if="!show">
+            <button @click="show = true">Add an ipsum!</button>
+        </div>
+        <div v-else>
+            <h3>Add your own Ipsum!</h3>
             <IpsumForm />
         </div>
     </section>
@@ -17,12 +21,25 @@ import IpsumDetail from './IpsumDetail.vue';
 import IpsumForm from './IpsumForm.vue';
 
 export default {
+  data() {
+    return {
+      show: false
+    };
+  },
+
   props: {
     ipsum: Object
   },
+
   components: {
     IpsumDetail,
     IpsumForm
+  },
+
+  methods: {
+    handleAdd(){
+      this.show = true;
+    }
   }
 };
 </script>
