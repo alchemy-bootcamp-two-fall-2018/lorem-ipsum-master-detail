78c95479f01159120abe2563d43ea42a52a76fd7
Abel Quintero <abel.j.quintero@gmail.com>
2018-11-28 23:38:21 -0800

    Add Toggle to Detail View to Show Form

--- src/components/AddIpsum.vue
+++ src/components/AddIpsum.vue
@@ -1,15 +1,63 @@
 <template>
-  <ul>
-        <h3>This is the AddIpsum component</h3>
-  </ul>
+  <section class="form-section">
+    <div v-if="!show">
+      <p>Click to create a new ipsum!</p>
+      <button @click="show = true">Add an Ipsum</button>
+    </div>
+    <div v-else>
+      <h2>Add a New Ipsum</h2>
+      <IpsumForm class="form"
+        :onAdd="handleAdd"
+        :onCancel="() => show = false"/>
+    </div>
+  </section>
 </template>
 
 <script>
+import IpsumForm from './IpsumForm.vue'
 export default {
-
-}
+  data() {
+    return {
+      show: false
+    };
+  },
+  components: {
+    IpsumForm
+  },
+  props: {
+    onAdd: Function
+  },
+  methods: {
+    handleAdd(ipsum) {
+      this.show = false;
+      this.onAdd(ipsum);
+    }
+  }
+};
 </script>
 
 <style>
+.form-section {
+    border: 3px solid gray;
+    margin: 5px 0px 15px 0px;
+    text-align: center;
+  }
+  button {
+    border: 2px solid black;
+    border-radius: 5px;
+    margin: 5px;
+    font-size: 0.8em;
+    padding: 4px;
+  }
+  h2 {
+    padding-bottom: 5px;
+    margin: 0px;
+    font-weight: 550;
+    color: navy;
+  }
+  .form {
+    margin: 0px auto;
+    padding: 0px;
+  }
 
 </style>
