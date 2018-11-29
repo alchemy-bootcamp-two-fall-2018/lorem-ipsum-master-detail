bcb8a75787b765e6b5aa0b04ea3b69eb72f8f053
Ivan <i.poblete915@gmail.com>
2018-11-28 16:16:14 -0800

    set up Ipsums.vue and Ipsums.api

---
+++ src/components/Ipsums.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul>
+       <Ipsum v-for="ipsum in ipsums"
+            :key="ipsum.title"
+            :ipsum="ipsum"/> 
+    </ul>
+</template>
+
+<script>
+import ipsumsApi from './services/ipsumsApi';
+
+export default {
+  data() {
+    return {
+      ipsums: ipsumsApi.getIpsums(),
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
