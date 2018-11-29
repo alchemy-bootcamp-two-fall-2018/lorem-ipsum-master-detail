16c06ef0e53865df22888f04179200a0b909ef0a
‘Michael <pickup.patterns@gmail.com>
2018-11-29 08:59:58 -0800

    update pckg json

---
+++ IpsumDetail.vue
@@ -0,0 +1,19 @@
+<template>
+    <li>
+        <div class="info">
+            <!-- <a href="{{ipsum.authorUrl}}" target="_blank"><h3>{{ipsum.title}}</h3></a> -->
+            <h3>I have a sentence here.</h3>
+            
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
