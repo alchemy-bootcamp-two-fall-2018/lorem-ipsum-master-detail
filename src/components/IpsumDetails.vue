80f52f0f7b4a8028a52fb3171184e58db238e6c7
Aaron Dennis <AMDennis1987@gmail.com>
2018-11-28 17:41:57 -0800

    took out my shot at editing

--- src/components/IpsumDetails.vue
+++ src/components/IpsumDetails.vue
@@ -1,37 +1,19 @@
 <template>
   <section>
-    <div v-if="edit">
-      <h2>Edit {{ipsum.title}}</h2>
-      <IpsumForm
-        :ipsumToUpdate="ipsum"
-        :ipsumTypes="ipsumTypes"
-        :onAdd="handleEdit"
-        :onCancel="() => edit = false"/>
-    </div>
     <IpsumList 
-      v-if="!edit"
-      :ipsum="ipsum"
-      :onEdit="() => edit = true"/>
+      :ipsum="ipsum"/>
   </section>
 </template>
 
 <script>
 import IpsumList from './IpsumList';
-import IpsumForm from './IpsumForm';
 export default {
-    data() {
-        return {
-            edit: false
-        };
-    },
     props: {
         ipsum: Object,
-        ipsumTypes: Array,
-        onEdit: Function
+        ipsumTypes: Array
     },
     components: {
-        IpsumList,
-        IpsumForm
+        IpsumList
     }
 };
 </script>
