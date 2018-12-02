5675b5da8bf84563b49351bc552f81cd77c52a66
Christina T. Herlocker <cherlocker3113@gmail.com>
2018-12-01 18:59:15 -0800

    correcting linting errors

--- src/components/IpsumList.vue
+++ src/components/IpsumList.vue
@@ -1,16 +1,16 @@
 <template>
-    
-INPROGRESSO
+    <h2>
+        INPROGRESSO - incomplete
+    </h2>
 </template>
 
 <script>
 
 import Ipsum from './Ipsums.vue';
 
-
 export default {
-
-}
+  Ipsum
+};
 </script>
 
 <style>
